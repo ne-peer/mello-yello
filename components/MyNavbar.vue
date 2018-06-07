ed235ce2e92a959777721b8af5a08c5503e1408a
ne-peer <mileage0110@gmail.com>
2018-06-07 18:02:48 +0900

    add: navbar

---
+++ components/MyNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+        <div class="uk-navbar-center">
+            <nuxt-link to="/" tag="a" class="uk-navbar-item uk-logo my-logo">Mellow yellow</nuxt-link>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.my-logo {
+  color: #FFF9C4;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  letter-spacing: 1px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div class="uk-background-cover my-background">
+        <header>
+            <my-navbar></my-navbar>
+        </header>
+        <nuxt/>
+    </div>
 </template>
+
+<script>
+import MyNavbar from "~/components/MyNavbar.vue";
+
+export default {
+  components: {
+    MyNavbar
+  }
+};
+</script>
+
+<style scoped>
+.my-background {
+  background-image: url(https://ne-peer.ams3.digitaloceanspaces.com/resource/melloyello-unsplash-min-blur.jpg);
+  height: 100vh;
+}
+</style>
