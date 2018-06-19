725015a0be6527e2ba47a85628c055f43b741e48
ne-peer <mileage0110@gmail.com>
2018-06-19 19:08:36 +0900

    fix: image

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
 
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
-                        <!-- <img src="https://pbs.twimg.com/media/DebJ9t5W4Aoka9o.jpg" alt="mello-yello"> -->
-                        <img src="http://via.placeholder.com/533x533" alt="mello-yello">
+                        <img src="https://pbs.twimg.com/media/DebJ9t5W4Aoka9o.jpg" alt="mello-yello">
+                        <!-- <img src="http://via.placeholder.com/533x533" alt="mello-yello"> -->
                     </div>
                     <div class="uk-card-body">
                         <h3 class="uk-card-title">Hello!</h3>
