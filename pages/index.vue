43e539b7fecca5969e250f9385b78d44a23d0f4f
ne-peer <mileage0110@gmail.com>
2018-06-20 15:52:19 +0900

    update: topをモジュール化

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,8 @@
         <div class="uk-margin-top uk-flex-center" uk-grid>
             <div class="uk-width-1-2@s" style="padding-right:20px">
 
-                <div class="uk-card uk-card-default">
-                    <div class="uk-card-media-top">
-                        <img src="https://pbs.twimg.com/media/DebJ9t5W4Aoka9o.jpg" alt="mello-yello">
-                        <!-- <img src="http://via.placeholder.com/533x533" alt="mello-yello"> -->
-                    </div>
-                    <div class="uk-card-body">
-                        <h3 class="uk-card-title">Hello!</h3>
-                        <p>Welcome to <a href="mello-yello.co">mello-yello.co</a>!!</p>
-                    </div>
+                <div>
+                  <welcome></welcome>
                 </div>
 
                 <div class="uk-margin-large">
@@ -24,11 +17,12 @@
 </template>
 
 <script>
+import Welcome from "~/components/Welcome.vue";
 import MemberList from "~/components/MemberList.vue";
 
 export default {
   components: {
-    MemberList
+    Welcome, MemberList
   }
 };
 </script>
