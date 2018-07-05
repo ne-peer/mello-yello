cbf83ea6d7317a35230ad6d4ac3e0b03e95f6d94
ne-peer <mileage0110@gmail.com>
2018-07-05 18:56:19 +0900

    update: welcome image random script

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -2,8 +2,7 @@
   <div>
     <div class="uk-card uk-card-default my-radius img-max-width">
         <div class="uk-card-media-top">
-            <img src="https://pbs.twimg.com/media/DebJ9t5W4Aoka9o.jpg" alt="mello-yello" class="my-radius-top">
-            <!-- <img src="http://via.placeholder.com/533x533" alt="mello-yello" class="my-radius-top"> -->
+            <img :src="imageUrl" alt="mello-yello" class="my-radius-top">
         </div>
         <div class="uk-card-body welcome">
             <h3 class="uk-card-title">Hello!</h3>
@@ -12,6 +11,36 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    const shuffle = array => {
+      let n = array.length,
+        t,
+        i;
+      while (n) {
+        i = Math.floor(Math.random() * n--);
+        t = array[n];
+        array[n] = array[i];
+        array[i] = t;
+      }
+      return array;
+    };
+
+    const list = [
+      "https://pbs.twimg.com/media/DebJ9t5W4Aoka9o.jpg",
+      "https://pbs.twimg.com/media/DhT4lc4UwAEWbwW.jpg"
+    ];
+
+    const val = shuffle(list).pop();
+
+    return {
+      imageUrl: val
+    };
+  }
+};
+</script>
 
 <style scoped>
 .img-max-width {
