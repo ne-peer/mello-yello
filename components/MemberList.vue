3d60d39f415ccf386c22df0ccde38ed37fdd2c21
ne-peer <mileage0110@gmail.com>
2018-07-05 17:59:56 +0900

    update: members list responsive

--- components/MemberList.vue
+++ components/MemberList.vue
@@ -1,35 +1,67 @@
 <template>
   <div class="uk-padding-small">
 
-    <div class="uk-grid-small uk-child-width-1-3@m uk-child-width-1-2@s" uk-grid uk-height-match="target: > div > .uk-card; row: false">
-      <div v-for="(mem, k) in members" :key="k">
-        <div class="uk-card uk-card-default my-radius">
-          <div class="uk-card-header">
-            <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-auto">
-                <template v-if="mem.tw">
-                  <img class="uk-border-circle" width="50" height="50" :src="'https://avatars.io/twitter/' + mem.tw">
-                </template>
-                <template v-else>
-                  <img class="uk-border-circle" width="50" height="50" src="~/assets/img/person_128_gray.jpg">
-                </template>
-              </div>
-              <div class="uk-width-expand">
-                  <h3 class="uk-card-title uk-margin-remove-bottom">{{ mem.name }}</h3>
-                  <p class="uk-margin-remove-top">
-                    <span class="uk-text-meta">{{ mem.date }}</span>
-                    <template v-if="mem.tw !== undefined">
-                      <span class="icon-minus" uk-icon="icon: minus; ratio: 0.6"></span>
-                      <a :href="'https://twitter.com/' + mem.tw" target="_blank" class="tw">@{{ mem.tw }}</a>
-                    </template>
-                  </p>
+    <!-- for desktop -->
+    <div class="uk-visible@s">
+      <div class="uk-grid-small uk-child-width-1-4@l uk-child-width-1-3@m uk-child-width-1-2@s" uk-grid uk-height-match="target: > div > .uk-card; row: false">
+        <div v-for="(mem, k) in members" :key="k">
+          <div class="uk-card uk-card-default my-radius">
+            <div class="uk-card-header">
+              <div class="uk-grid-small uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                  <template v-if="mem.tw">
+                    <img class="uk-border-circle" width="50" height="50" :src="'https://avatars.io/twitter/' + mem.tw">
+                  </template>
+                  <template v-else>
+                    <img class="uk-border-circle" width="50" height="50" src="~/assets/img/person_128_gray.jpg">
+                  </template>
+                </div>
+                <div class="uk-width-expand">
+                    <h3 class="uk-card-title uk-margin-remove-bottom">{{ mem.name }}</h3>
+                    <p class="uk-margin-remove-top">
+                      <span class="uk-text-meta">{{ mem.date }}</span>
+                      <template v-if="mem.tw !== undefined">
+                        <span class="icon-minus" uk-icon="icon: minus; ratio: 0.6"></span>
+                        <a :href="'https://twitter.com/' + mem.tw" target="_blank" class="tw">@{{ mem.tw }}</a>
+                      </template>
+                    </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
+
+    <!-- for smartphone -->
+    <div class="uk-hidden@s">
+      <div class="uk-grid-small uk-child-width-1-3 uk-margin-top" uk-grid>
+        <div v-for="(mem, k) in members" :key="k">
+          <div>
+            <div class="uk-card uk-card-hover">
+              <div class="uk-card-media-top uk-flex uk-flex-center" style="height:60px;">
+                <template v-if="mem.tw">
+                  <img class="uk-border-circle" height="60" width="60" :src="'https://avatars.io/twitter/' + mem.tw">
+                </template>
+                <template v-else>
+                  <img class="uk-border-circle" width="60" height="60" src="~/assets/img/person_128_gray.jpg">
+                </template>
+              </div>
+              <div class="uk-card-body uk-text-center uk-padding-remove-horizontal uk-padding-remove-top">
+                <p style="padding-top:16px">
+                  <span class="uk-text-meta">{{ mem.name }}</span>
+                  <template v-if="mem.tw !== undefined">
+                    <br>
+                    <a :href="'https://twitter.com/' + mem.tw" target="_blank" class="tw-small">@{{ mem.tw }}</a>
+                  </template>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -127,7 +159,7 @@ export default {
           name: "ふじおか",
           date: "2018.06.16",
           tw: "fuziokasouth"
-        },
+        }
       ]
     };
   }
@@ -160,6 +192,10 @@ export default {
 
 a.tw {
   font-size: 12px;
+}
+
+a.tw-small {
+  font-size: 8px;
 }
 
 .uk-card-default .uk-card-header {
